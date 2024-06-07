b15542ff1b14710e7f7e4e192ef3e7065c4ef931
billihazero <billihazero@gmail.com>
2024-06-07 15:55:07 +0900

    userResponse 추가

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -3,6 +3,8 @@ package com.group.libraryapp.controller.user;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.group.libraryapp.dto.user.response.UserResponse;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +16,19 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest;
 public class UserController {
 
     private final List<User> users = new ArrayList<>();
+
     @PostMapping("/user")
     public void saveUser(@RequestBody UserCreateRequest request) {
         users.add(new User(request.getName(), request.getAge()));
     }
 
-
+    @GetMapping("/user")
+    public List<UserResponse> getUsers(){
+        List<UserResponse> responses = new ArrayList<>();
+        for(int i=0; i<users.size(); i++){
+            responses.add(new UserResponse(i+1, users.get(i)));
+        }
+        return responses;
+    }
 
 }
