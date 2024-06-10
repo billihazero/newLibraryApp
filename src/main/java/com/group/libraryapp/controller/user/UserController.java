891eeea272381d7e303cbd507d1ddfe3261cfa06
billihazero <billihazero@gmail.com>
2024-06-10 17:50:15 +0900

    db연결

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -1,9 +1,13 @@
 package com.group.libraryapp.controller.user;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.group.libraryapp.dto.user.response.UserResponse;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,20 +19,31 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest;
 @RestController
 public class UserController {
 
-    private final List<User> users = new ArrayList<>();
+    private JdbcTemplate jdbcTemplate;
+
+    public UserController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @PostMapping("/user")
     public void saveUser(@RequestBody UserCreateRequest request) {
-        users.add(new User(request.getName(), request.getAge()));
+        String sql = "INSERT INTO user (name, age) VALUES (?, ?)";
+        jdbcTemplate.update(sql, request.getName(), request.getAge());
     }
 
     @GetMapping("/user")
-    public List<UserResponse> getUsers(){
-        List<UserResponse> responses = new ArrayList<>();
-        for(int i=0; i<users.size(); i++){
-            responses.add(new UserResponse(i+1, users.get(i)));
-        }
-        return responses;
+    public List<UserResponse> getUsers() {
+        String sql = "SELECT * FROM user";
+        return jdbcTemplate.query(sql, new RowMapper<UserResponse>() {
+            @Override
+            public UserResponse mapRow(ResultSet rs, int rowNum) throws SQLException {
+                long id = rs.getLong("id");
+                String name = rs.getString("name");
+                int age = rs.getInt("age");
+                return new UserResponse(id, name, age);
+            }
+
+        });
     }
 
 }
